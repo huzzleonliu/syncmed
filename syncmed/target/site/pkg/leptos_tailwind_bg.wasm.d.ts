8d9d2a04bf382f6b9098a2a18f5d6c60d1f96aef
huzz <huzzleonliu@foxmail.com>
2026-03-17 11:57:21 +0100

    compiled files added

---
+++ syncmed/target/site/pkg/leptos_tailwind_bg.wasm.d.ts
@@ -0,0 +1,37 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const hydrate: () => void;
+export const __wbg_intounderlyingbytesource_free: (a: number, b: number) => void;
+export const intounderlyingbytesource_autoAllocateChunkSize: (a: number) => number;
+export const intounderlyingbytesource_cancel: (a: number) => void;
+export const intounderlyingbytesource_pull: (a: number, b: any) => any;
+export const intounderlyingbytesource_start: (a: number, b: any) => void;
+export const intounderlyingbytesource_type: (a: number) => number;
+export const __wbg_intounderlyingsink_free: (a: number, b: number) => void;
+export const intounderlyingsink_abort: (a: number, b: any) => any;
+export const intounderlyingsink_close: (a: number) => any;
+export const intounderlyingsink_write: (a: number, b: any) => any;
+export const __wbg_intounderlyingsource_free: (a: number, b: number) => void;
+export const intounderlyingsource_cancel: (a: number) => void;
+export const intounderlyingsource_pull: (a: number, b: any) => any;
+export const wasm_bindgen_9c06d9feafd6a0a5___closure__destroy___dyn_core_caf160d1939785e___ops__function__FnMut__wasm_bindgen_9c06d9feafd6a0a5___JsValue____Output_______: (a: number, b: number) => void;
+export const wasm_bindgen_9c06d9feafd6a0a5___closure__destroy___dyn_core_caf160d1939785e___ops__function__FnMut__wasm_bindgen_9c06d9feafd6a0a5___JsValue____Output________1_: (a: number, b: number) => void;
+export const wasm_bindgen_9c06d9feafd6a0a5___closure__destroy___dyn_core_caf160d1939785e___ops__function__FnMut__web_sys_da700e4cd7a8986f___features__gen_Event__Event____Output_______: (a: number, b: number) => void;
+export const wasm_bindgen_9c06d9feafd6a0a5___closure__destroy___dyn_core_caf160d1939785e___ops__function__FnMut_____Output_______: (a: number, b: number) => void;
+export const wasm_bindgen_9c06d9feafd6a0a5___closure__destroy___dyn_core_caf160d1939785e___ops__function__FnMut__wasm_bindgen_9c06d9feafd6a0a5___JsValue____Output___core_caf160d1939785e___result__Result_____wasm_bindgen_9c06d9feafd6a0a5___JsError___: (a: number, b: number) => void;
+export const wasm_bindgen_9c06d9feafd6a0a5___convert__closures_____invoke___wasm_bindgen_9c06d9feafd6a0a5___JsValue__core_caf160d1939785e___result__Result_____wasm_bindgen_9c06d9feafd6a0a5___JsError___true_: (a: number, b: number, c: any) => [number, number];
+export const wasm_bindgen_9c06d9feafd6a0a5___convert__closures_____invoke___js_sys_cd0cb67aa981654f___Function_fn_wasm_bindgen_9c06d9feafd6a0a5___JsValue_____wasm_bindgen_9c06d9feafd6a0a5___sys__Undefined___js_sys_cd0cb67aa981654f___Function_fn_wasm_bindgen_9c06d9feafd6a0a5___JsValue_____wasm_bindgen_9c06d9feafd6a0a5___sys__Undefined_______true_: (a: number, b: number, c: any, d: any) => void;
+export const wasm_bindgen_9c06d9feafd6a0a5___convert__closures_____invoke___wasm_bindgen_9c06d9feafd6a0a5___JsValue______true_: (a: number, b: number, c: any) => void;
+export const wasm_bindgen_9c06d9feafd6a0a5___convert__closures_____invoke___wasm_bindgen_9c06d9feafd6a0a5___JsValue______true__1_: (a: number, b: number, c: any) => void;
+export const wasm_bindgen_9c06d9feafd6a0a5___convert__closures_____invoke___web_sys_da700e4cd7a8986f___features__gen_Event__Event______true_: (a: number, b: number, c: any) => void;
+export const wasm_bindgen_9c06d9feafd6a0a5___convert__closures_____invoke___bool__true_: (a: number, b: number) => number;
+export const wasm_bindgen_9c06d9feafd6a0a5___convert__closures_____invoke_______true_: (a: number, b: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
